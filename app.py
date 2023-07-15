1e84ea0f74d5aa11104b8571f42783b9a345ae3c
Amvi06 <amrita.github06@gmail.com>
2023-07-15 18:25:17 -0400

    initial files

---
+++ app.py
@@ -0,0 +1,30 @@
+#Dependencies
+from flask import Flask,render_template,session,redirect,url_for,jsonify,send_from_directory
+
+
+# App stuff
+app = Flask(__name__,template_folder='template')
+
+#Routers
+@app.route("/",methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/cov",methods=["GET"])
+def cov():
+    return render_template("cov_letter.html")
+
+@app.route("/project",methods=["GET"])
+def proj():
+    return render_template("project.html")
+
+@app.route("/echo/<text>")
+def repeat(text):
+    return render_template("text.html",txt=text)
+
+
+
+#Run
+if __name__ == "__main__":
+    app.debug= True
+    app.run(host="0.0.0.0",port=5002)
